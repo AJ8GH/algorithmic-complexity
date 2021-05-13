e8f23050eabb01f80d752b8f9390cb461cbd7ab3
Adam Jonas <adamjonas25@gmail.com>
2021-05-13 10:32:44 +0100

    Define finish() method to end timer and record time

--- test/CodeTimer.test.js
+++ test/CodeTimer.test.js
@@ -8,12 +8,27 @@ describe('CodeTimer', () => {
     it('records the start time', () => {
       const codeTimer = new CodeTimer()
 
-      const time = new Date().getTime()
-      sinon.useFakeTimers(time)
-
+      const startTime = sinon.useFakeTimers(new Date().getTime())
       codeTimer.start()
 
-      expect(codeTimer.startTime).to.equal(time)
+      expect(codeTimer.startTime).to.equal(startTime.now)
+
+      startTime.restore()
+    })
+  })
+
+  describe('#finish()', () => {
+    it('records the finish time', () => {
+      const codeTimer = new CodeTimer()
+
+      const startTime = sinon.useFakeTimers(new Date().getTime())
+      codeTimer.start()
+
+      const finishTime = sinon.useFakeTimers(startTime.now + 1000)
+      codeTimer.finish()
+
+      expect(codeTimer.finishTime).to.equal(finishTime.now)
+      finishTime.restore()
     })
   })
 })
