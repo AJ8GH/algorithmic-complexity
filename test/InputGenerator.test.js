dc5cc1f719c97b215c3a48580b76d67caa3ced5c
Adam Jonas <adamjonas25@gmail.com>
2021-05-13 23:19:36 +0100

    Make generate input method fill the array

--- test/InputGenerator.test.js
+++ test/InputGenerator.test.js
@@ -1,9 +1,22 @@
 import { expect } from 'chai'
-import GenerateInputArray from '../lib/InputGenerator.js'
+import InputGenerator from '../lib/InputGenerator.js'
 
 describe('GenerateInputArray()', () => {
   it('generates an array of its argument length', () => {
-    const array = GenerateInputArray(5)
-    expect(array).to.have.lengthOf(5)
+    const inputGenerator = new InputGenerator()
+    const array = inputGenerator.generate(50000)
+
+    expect(array).to.have.lengthOf(50000)
+  })
+
+  it('fills the array', () => {
+    const inputGenerator = new InputGenerator()
+    const array = inputGenerator.generate(100000)
+
+    expect(array[0]).to.exist
+    expect(array[25000]).to.exist
+    expect(array[50000]).to.exist
+    expect(array[75000]).to.exist
+    expect(array[99999]).to.exist
   })
 })
