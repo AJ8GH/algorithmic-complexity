7d1554b5e9f5d498dbb3e787b10e53e0dab4d53f
Adam Jonas <adamjonas25@gmail.com>
2021-05-13 21:34:05 +0100

    Define Input array Generator function

---
+++ standardFunctions/Reverse.js
@@ -0,0 +1,17 @@
+import CodeTimer from '../lib/CodeTimer.js'
+import GenerateInputArray from '../lib/InputGenerator.js'
+
+const codeTimer = new CodeTimer()
+
+// function TimeReverse (array) {
+//   codeTimer.start()
+//   array.reverse()
+//   codeTimer.finish()
+// }
+
+// const inputArray = GenerateInputArray()
+
+// TimeReverse(inputArray)
+// console.log(codeTimer.runTime())
+// console.log(codeTimer.startTime)
+// console.log(codeTimer.finishTime)
---
+++ test/InputGenerator.test.js
@@ -0,0 +1,9 @@
+import { expect } from 'chai'
+import GenerateInputArray from '../lib/InputGenerator.js'
+
+describe('GenerateInputArray()', () => {
+  it('generates an array of its argument length', () => {
+    const array = GenerateInputArray(5)
+    expect(array.length).lessThanOrEqual(5)
+  })
+})
