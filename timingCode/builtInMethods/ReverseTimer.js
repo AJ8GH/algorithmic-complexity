9c1c20f5565c55e439990a2669fb9792b4f97b12
Adam Jonas <adamjonas25@gmail.com>
2021-05-14 13:32:40 +0100

    Fix bug in Algorithm runner options passed to code timer

--- timingCode/builtInMethods/ReverseTimer.js
+++ timingCode/builtInMethods/ReverseTimer.js
@@ -1,17 +1,5 @@
-import CodeTimer from '../../lib/CodeTimer.js'
+import AlgorithmRunner from '../../lib/AlgorithmRunner.js'
 
-function createIntputSample (initialSize) {
-  const inputSample = []
-  for (let size = initialSize; size <= initialSize * 20; size += initialSize) {
-    inputSample.push(size)
-  }
-  return inputSample
-}
+const algorithmRunner = new AlgorithmRunner()
 
-function algorithmRunner (options) {
-  const codeTimer = new CodeTimer(options.method)
-  const inputSample = createIntputSample(options.size)
-  inputSample.forEach(inputArray => codeTimer.time(inputArray))
-}
-
-algorithmRunner({ method: [].reverse, size: 1000000 })
+algorithmRunner.run({ method: [].reverse, size: 100000 })
