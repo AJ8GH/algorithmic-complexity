3de52639765be5990c1eebccd29de255c83b7973
Adam Jonas <adamjonas25@gmail.com>
2021-05-18 18:00:20 +0100

    Refactor find duplicates alogrithm

--- customAlgorithms/lib/FindDuplicates.js
+++ customAlgorithms/lib/FindDuplicates.js
@@ -1,15 +1,7 @@
 export default function findDuplicates (array) {
-  const duplicates = array.map((element) => {
+  const duplicates = array.map(element => {
     const count = []
-
-    array.forEach(el => {
-      if (el === element) {
-        count.push(el)
-      }
-    })
-
-    console.log(count.length)
-
+    array.forEach(el => { if (el === element) { count.push(el) } })
     if (count.length > 1) { return element }
   })
 
