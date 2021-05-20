929ca8281d15d5a1e97a6aa6972578af27f38baa
Adam Jonas <adamjonas25@gmail.com>
2021-05-20 23:14:34 +0100

    Implement shuffle algorithm

--- algorithms/test/shuffle.test.js
+++ algorithms/test/shuffle.test.js
@@ -9,6 +9,17 @@ describe('#shuffle()', () => {
 
   it('shuffles a 2 element array', () => {
     Sinon.stub(Math, 'random').returns(0.5)
+
     expect(shuffle([1, 2])).to.deep.equal([2, 1])
+
+    Sinon.restore()
+  })
+
+  it('shuffles a 3 element array', () => {
+    Sinon.stub(Math, 'random').returns(0.5)
+
+    expect(shuffle([1, 2, 3])).to.deep.equal([2, 3, 1])
+
+    Sinon.restore()
   })
 })
