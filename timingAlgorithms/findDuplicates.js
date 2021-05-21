7ec3bcc59e85fc5df416e26b4c13150ce5f56e09
Adam Jonas <adamjonas25@gmail.com>
2021-05-21 11:13:54 +0100

    Retime algorithms with control inputs at the beginning, to remove initial spike

--- timingAlgorithms/findDuplicates.js
+++ timingAlgorithms/findDuplicates.js
@@ -3,4 +3,4 @@ import AlgoRunner from '../codeTimer/lib/AlgoRunner.js'
 
 const algoRunner = new AlgoRunner()
 
-algoRunner.run({ method: findDuplicates, size: 5000, custom: true })
+algoRunner.run({ method: findDuplicates, size: 4000, custom: true })
