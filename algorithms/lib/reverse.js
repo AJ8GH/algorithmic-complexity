debb6334ccb16c715c76e7ea3a95673ed52ccac7
Adam Jonas <adamjonas25@gmail.com>
2021-05-21 09:04:23 +0100

    Implement reverse algorithm for 2 element array

--- algorithms/lib/reverse.js
+++ algorithms/lib/reverse.js
@@ -1,3 +1,4 @@
 export default function reverse (array) {
+  if (array.length === 2) { return [array[1], array[0]] }
   return array
 }
--- algorithms/test/reverse.test.js
+++ algorithms/test/reverse.test.js
@@ -1,8 +1,12 @@
 import { expect } from 'chai'
-import reverse from '../lib/customReverse.js'
+import reverse from '../lib/reverse.js'
 
 describe('#reverse()', () => {
   it('returns a 1 element array', () => {
     expect(reverse([1])).to.deep.equal([1])
+  })
+
+  it('reverses a 2 element array', () => {
+    expect(reverse([1, 2])).to.deep.equal([2, 1])
   })
 })
