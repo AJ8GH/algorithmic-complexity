7d839fb9a7be349185570c83569343ea692c2acb
Adam Jonas <adamjonas25@gmail.com>
2021-06-06 20:39:02 +0100

    Implement stock picker algorithm

--- algorithms/lib/stockPicker.js
+++ algorithms/lib/stockPicker.js
@@ -1,3 +1,11 @@
-export default function stockPicker (array) {
-  return array[1] - array[0]
+export default function stockPicker (prices) {
+  let maxProfit = prices[0] - prices[1]
+
+  prices.forEach((buyPrice, buyIndex) => {
+    for (let i = buyIndex + 1; i < prices.length; i++) {
+      const profit = prices[i] - buyPrice
+      if (profit > maxProfit) { maxProfit = profit }
+    }
+  })
+  return maxProfit
 }
--- algorithms/test/stockPicker.test.js
+++ algorithms/test/stockPicker.test.js
@@ -5,4 +5,12 @@ describe('#stockPicker()', () => {
   it('knows the profit when there are only 2 prices', () => {
     expect(stockPicker([1, 5])).to.equal(4)
   })
+
+  it('knows the profit when there are 3 prices', () => {
+    expect(stockPicker([1, 5, 10])).to.equal(9)
+  })
+
+  it('knows the profit when there are 3 prices', () => {
+    expect(stockPicker([5, 2, 7, 3, 5, 5, 9, 3, 1, 2, 6, 4])).to.equal(7)
+  })
 })
